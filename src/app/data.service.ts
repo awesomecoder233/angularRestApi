7f711863c696ca0fac7e162c3fd1a7810d42b6a3
awesomecoder233 <tommount58@gmail.com>
2021-08-28 15:47:54 +0200

    call restful_fake_api

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,6 +6,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  private REST_API_SERVER = "http://localhost:3000";
+  private REST_API_SERVER = "http://localhost:3000/products";
   constructor(private httpClient : HttpClient) { }
+  public sendGetRequest() {
+    return this.httpClient.get(this.REST_API_SERVER);
+  }
 }
