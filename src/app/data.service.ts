9dba67972064d46f5398cc4d2fac68d4068d9fa4
awesomecoder233 <tommount58@gmail.com>
2021-08-28 15:36:06 +0200

    import and inject HttpClient

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  private REST_API_SERVER = "http://localhost:3000";
+  constructor(private httpClient : HttpClient) { }
 }
