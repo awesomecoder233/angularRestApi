0c057e60c10de928d00be8df2ce58be66496f80d
awesomecoder233 <tommount58@gmail.com>
2021-08-30 07:08:12 +0200

    Unsubscribing from HttpClient Observables with RxJS takeUntil()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../data.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   products = [] as any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private dataService: DataService) { }
+  
 
   ngOnInit() {
 
-    this.dataService.sendGetRequest().subscribe((data)=>{
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((data)=>{
       console.log(data);
       this.products = data;
     })  
+  }
+  ngOnDestroy() {
+    throw new Error('Method not implemented.');
   }
 
 }
